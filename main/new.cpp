c88f949bf85f57ad7b2375f256c094bd2605a65f
shashwathrajkumar <shashwath.rajkumar@gmail.com>
2025-03-13 10:47:57 +0000

    Task 5 new cpp file

---
+++ main/new.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World, PES1UG22CS555 - Task 5 - Pipeline building\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
