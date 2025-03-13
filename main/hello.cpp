deded38a20dbabf5f7de636e94464ed69834f52c
shashwathrajkumar <shashwath.rajkumar@gmail.com>
2025-03-13 10:28:45 +0000

    automatic commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World, PES1UG22CS555\n";
+  cout << "Hello, World, PES1UG22CS555 - Automatic commit\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
